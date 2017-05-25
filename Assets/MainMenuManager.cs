9a066a26cd3a6858ce600acf86c5ff64d1099364
Julia <julia141407@gmail.com>
2017-05-25 14:56:52 +0300

    first commit

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+    public GameObject _panel = null;
+
+	public bool PanelForAbout = false;
+	public void Settings()
+	{
+		
+	}
+
+	public void GamePlay()
+	{
+		SceneManager.LoadScene ("Game Scena");
+	}
+
+	private void About()
+	{
+        if (_panel = null)
+        {
+            _panel.SetActive(PanelForAbout);
+        }
+    }
+	private void close_panel(){
+        PanelForAbout = false;
+        _panel.SetActive(PanelForAbout);
+        // onClick.button ("close") = PanelForAbout.setActive (false);
+    }
+
+		
+}
